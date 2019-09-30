133310d811c02fe2eadf98c7144c6bee6189c2c6
Ahish9009 <ahishjayant9009@gmail.com>
2019-09-30 15:40:19 +0530

    Updated backup

--- Backup/AhishDeshpande_2018102022/rc_script.tcl
+++ Backup/AhishDeshpande_2018102022/rc_script.tcl
@@ -1,17 +1,18 @@
 set_db lib_search_path ./lib/90
 set_db library slow.lib
 set_db hdl_search_path ./
+read_hdl {ripple4bit.v}
 read_hdl {fulladder1bit.v}
 elaborate
 read_sdc ./constraints_top.sdc
 synthesize -to_mapped -effort medium
 write_sdf -timescale ns -nonegchecks -recrem split -edges check_edge > delays.sdf
 
-write_hdl > fulladder1bit_netlist.v
-write_sdc > fulladder1bit_sdc.sdc
+write_hdl > ripple4bit_netlist.v
+write_sdc > ripple4bit_sdc.sdc
 
 gui_show
-report timing -unconstrained > fulladder1bit_timing.rep
-report power > fulladder1bit_power.rep
-report area > fulladder1bit_cell.rep
-report gates > fulladder1bit_gates.rep
+report timing -unconstrained > ripple4bit_timing.rep
+report power > ripple4bit_power.rep
+report area > ripple4bit_cell.rep
+report gates > ripple4bit_gates.rep
